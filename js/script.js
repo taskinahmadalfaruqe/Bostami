521a5780406eba01f796bbc21995c88fcb53c73f
TASKIN AHMAD AL FARUQE <119452412+taskinahmadalfaruqe@users.noreply.github.com>
2023-03-21 01:20:33 +0600

    done

--- js/script.js
+++ js/script.js
@@ -17,8 +17,34 @@ $(function(){
         slidesToScroll: 1,
         arrows:false,
         autoplay:true,
-        
-      });
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 5,
+                slidesToScroll: 1,
+                infinite: true,
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+              }
+            },
+            {
+              breakpoint: 640,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+        });
 
     //   YEAR 
     let FullYear= new Date().getFullYear();
